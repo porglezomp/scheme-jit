119a0b9e5b212e4c6cb77a5e41a9b1be3d86a280
James Perretta <jameslp@umich.edu>
2018-10-25 14:06:23 -0400

    Added SPair type. Converted parser tests to use python unittest. Fixed mypy errors.

--- scheme.py
+++ scheme.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import List, Optional, Tuple, Iterable
+from typing import List, Optional, Tuple, Iterable, Sequence, Union
 
 
 @dataclass
@@ -28,32 +28,104 @@ class SSym(SExp):
 
 @dataclass
 class SVect(SExp):
-    """An n element vector"""
+    """An n element vector
+
+    >>> vect = SVect([
+    ...     SNum(42), SSym('spam'),
+    ...     SVect([SNum(43), SNum(44)])
+    ... ])
+    >>> str(vect)
+    '[42 spam [43 44]]'
+    >>>
+    >>> str(SVect([]))
+    '[]'
+    """
     items: List[SExp]
 
-    def to_list_items(self) -> Optional[List[SExp]]:
-        if len(self.items) == 0:
-            return []
-        if len(self.items) != 2:
-            return None
-        rest_items = self.items[1].to_list_items()
-        if rest_items is None:
-            return None
-        return [self.items[0]] + rest_items
-
     def __str__(self) -> str:
-        items = self.to_list_items()
-        if items is not None:
-            if len(items) == 2 and items[0] == SSym('quote'):
-                return f"'{items[1]}"
-            return f"({' '.join(str(i) for i in items)})"
         return f"[{' '.join(str(i) for i in self.items)}]"
 
 
-def to_slist(x: Iterable[SExp]) -> SExp:
-    acc = SVect([])
+@dataclass
+class SPair(SExp):
+    """A scheme pair.
+
+    >>> single_pair = SPair(SNum(42), SSym('spam'))
+    >>> str(single_pair)
+    '(42 . spam)'
+    >>> list(single_pair)
+    [SNum(value=42), SSym(name='spam')]
+
+    >>> nested_pair = SPair(SNum(42), SPair(SSym('egg'), SSym('spam')))
+    >>> str(nested_pair)
+    '(42 . (egg . spam))'
+    >>> list(nested_pair)
+    [SNum(value=42), SSym(name='egg'), SSym(name='spam')]
+
+    >>> s_list = SPair(SNum(42), SPair(SSym('egg'), Nil))
+    >>> str(s_list)
+    '(42 egg)'
+    >>> list(s_list)
+    [SNum(value=42), SSym(name='egg')]
+    """
+    first: SExp
+    second: SExp
+
+    def is_list(self):
+        if self.second is Nil:
+            return True
+
+        if not isinstance(self.second, SPair):
+            return False
+
+        return self.second.is_list()
+
+    def __iter__(self):
+        return PairIter(self)
+
+    def __str__(self) -> str:
+        if self.is_list():
+            return f"({' '.join((str(item) for item in self))})"
+
+        return f"({str(self.first)} . {str(self.second)})"
+
+
+class PairIter:
+    def __init__(self, pair: SPair):
+        self._expr: SExp = pair
+
+    def __next__(self):
+        if self._expr is Nil:
+            raise StopIteration
+
+        if not isinstance(self._expr, SPair):
+            val = self._expr
+            self._expr = Nil
+            return val
+
+        val = self._expr.first
+        self._expr = self._expr.second
+
+        return val
+
+    def __iter__(self):
+        return self
+
+
+class NilType(SExp):
+    pass
+
+
+Nil = NilType()
+
+SList = Union[SPair, NilType]
+
+
+def to_slist(x: Sequence[SExp]) -> SList:
+    acc: SList = Nil
     for item in reversed(x):
-        acc = SVect([item, acc])
+        acc = SPair(item, acc)
+
     return acc
 
 
@@ -70,7 +142,7 @@ def parse(x: str) -> List[SExp]:
 
     def parse(tokens: List[str]) -> Tuple[SExp, List[str]]:
         if not tokens:
-            raise "Parse Error"
+            raise Exception("Parse Error")
         elif tokens[0] == "'":
             item, tokens = parse(tokens[1:])
             return to_slist([SSym("quote"), item]), tokens
--- test_parser.py
+++ test_parser.py
@@ -1,52 +1,69 @@
+import unittest
+
 import scheme
-from scheme import SSym, SVect, SNum
+from scheme import SSym, SVect, SNum, SPair, Nil
 
 
-def test_to_slist():
-    assert scheme.to_slist([SSym(1), SSym(2), SSym(3)]) == SVect([
-        SSym(1),
-        SVect([
-            SSym(2),
-            SVect([
-                SSym(3),
-                SVect([]),
-            ]),
-        ]),
-    ])
+class ParserTestCase(unittest.TestCase):
+    def test_to_slist(self):
+        self.assertEqual(
+            scheme.to_slist([SSym(1), SSym(2), SSym(3)]),
+            SPair(
+                SSym(1),
+                SPair(
+                    SSym(2),
+                    SPair(
+                        SSym(3),
+                        Nil,
+                    ),
+                ),
+            )
+        )
+
+    def test_parse_atoms(self):
+        self.assertEqual(scheme.parse("hi"), [SSym("hi")])
+        self.assertEqual(
+            scheme.parse("hi hey hoi"),
+            [
+                SSym("hi"),
+                SSym("hey"),
+                SSym("hoi"),
+            ]
+        )
+        self.assertEqual(scheme.parse("42 foo"), [SNum(42), SSym("foo")])
+
+    def test_parse_list(self):
+        self.assertEqual(scheme.parse("()"), [Nil])
+        self.assertEqual(
+            scheme.parse("(1 2 3)"),
+            [scheme.to_slist([SNum(1), SNum(2), SNum(3)])],
+        )
+        self.assertEqual(
+            scheme.parse("(if (< x 0) 1 2)"),
+            [
+                scheme.to_slist([
+                    SSym("if"),
+                    scheme.to_slist([SSym("<"), SSym("x"), SNum(0)]),
+                    SNum(1),
+                    SNum(2),
+                ]),
+            ]
+        )
+
+    def test_vector(self):
+        self.assertEqual(scheme.parse("[]"), [SVect([])])
+        self.assertEqual(
+            scheme.parse("[1 [2 [3 []]]]"),
+            [SVect([SNum(1), SVect([SNum(2), SVect([SNum(3), SVect([])])])])]
+        )
+
+    def test_quote(self):
+        self.assertEqual(
+            scheme.parse("'(1 2 3)"), scheme.parse("(quote (1 2 3))"))
+
+        # fixme?
+        # self.assertEqual(str(scheme.parse("(quote (1 2 3))")[0]), "'(1 2 3)")
 
 
-def test_parse_atoms():
-    assert scheme.parse("hi") == [SSym("hi")]
-    assert scheme.parse("hi hey hoi") == [
-        SSym("hi"),
-        SSym("hey"),
-        SSym("hoi"),
-    ]
-    assert scheme.parse("42 foo") == [SNum(42), SSym("foo")]
-
-
-def test_parse_list():
-    assert scheme.parse("()") == [SVect([])]
-    assert scheme.parse("(1 2 3)") == [
-        scheme.to_slist([SNum(1), SNum(2), SNum(3)]),
-    ]
-    assert scheme.parse("(if (< x 0) 1 2)") == [
-        scheme.to_slist([
-            SSym("if"),
-            scheme.to_slist([SSym("<"), SSym("x"), SNum(0)]),
-            SNum(1),
-            SNum(2),
-        ]),
-    ]
-
-
-def test_vector():
-    assert scheme.parse("[]") == [SVect([])]
-    assert scheme.parse("[1 [2 [3 []]]]") == [
-        SVect([SNum(1), SVect([SNum(2), SVect([SNum(3), SVect([])])])]),
-    ]
-
-
-def test_quote():
-    assert scheme.parse("'(1 2 3)") == scheme.parse("(quote (1 2 3))")
-    assert str(scheme.parse("(quote (1 2 3))")[0]) == "'(1 2 3)"
+if __name__ == '__main__':
+    unittest.main()
