5ae87c5939c9d4f76c6cd1d22126375d989327a9
C Jones <code@calebjones.net>
2018-11-17 17:34:02 -0500

    Implement the builtin functions

--- runner.py
+++ runner.py
@@ -3,6 +3,7 @@ from typing import Dict, List
 import bytecode
 import scheme
 from bytecode import BasicBlock, Binop, Function, Inst, Var
+from emit_IR import FunctionEmitter
 from scheme import Nil, SFunction, SSym, Value
 
 
@@ -22,8 +23,8 @@ def inst_function(
 def add_intrinsics(env: Dict[SSym, Value]) -> None:
     """Add intrinsics to the environment."""
     result = Var('result')
-    env[SSym('typeof')] = inst_function(
-        SSym('typeof'), [Var('x')],
+    env[SSym('inst/typeof')] = inst_function(
+        SSym('inst/typeof'), [Var('x')],
         bytecode.TypeofInst(result, Var('x')))
     env[SSym('inst/alloc')] = inst_function(
         SSym('inst/alloc'), [Var('n')],
@@ -51,8 +52,8 @@ def add_intrinsics(env: Dict[SSym, Value]) -> None:
     env[SSym('inst/number<')] = inst_function(
         SSym('inst/number<'), [Var('a'), Var('b')],
         bytecode.BinopInst(result, Binop.NUM_LT, Var('a'), Var('b')))
-    env[SSym('trap')] = inst_function(
-        SSym('trap'), [],
+    env[SSym('inst/trap')] = inst_function(
+        SSym('inst/trap'), [],
         bytecode.TrapInst("(trap)"),
         should_return=False)
 
@@ -60,7 +61,60 @@ def add_intrinsics(env: Dict[SSym, Value]) -> None:
 def add_builtins(env: Dict[SSym, Value]) -> None:
     """Add builtins to the environment."""
     add_intrinsics(env)  # @TODO: Don't do this, for greater flexibility?
-    raise NotImplementedError("builtins")
+    code = scheme.parse("""
+    (define (trap) (inst/trap))
+    (define (assert b) (if b 0 (trap)))
+    (define (typeof x) (inst/typeof x))
+
+    (define (pointer= a b) (inst/pointer= a b))
+    (define (symbol= a b)
+      (assert (inst/symbol= (typeof a) 'symbol))
+      (assert (inst/symbol= (typeof b) 'symbol))
+      (inst/symbol= a b))
+    (define (number= a b)
+      (assert (symbol= (typeof a) 'number)
+      (assert (symbol= (typeof b) 'number)
+      (inst/number= a b))))
+
+    (define (number< a b)
+      (assert (symbol= (typeof a) 'number))
+      (assert (symbol= (typeof b) 'number))
+      (inst/number< a b))
+
+    (define (vector-length v)
+      (assert (symbol= (typeof v) 'vector))
+      (inst/length v))
+
+    (define (vector-index v n)
+      (assert (symbol= (typeof v) 'vector))
+      (assert (symbol= (typeof n) 'number))
+      (assert (number< -1 n))
+      (assert (number< n (vector-length v)))
+      (inst/load v n))
+
+    (define (vector-set! v n x)
+      (assert (symbol= (typeof v) 'vector))
+      (assert (symbol= (typeof n) 'number))
+      (assert (number< -1 n))
+      (assert (number< n (vector-length v)))
+      (inst/store v n x))
+
+    (define (vector-make/recur len idx v x)
+      (vector-set! v idx x)
+      (if (number= len (+ idx 1))
+        v
+        (vector-make/recur len (+ idx 1) v x)))
+
+    (define (vector-make n x)
+      (assert (symbol= (typeof n) 'number))
+      (assert (number< -1 n))
+      (if (number= n 0)
+        (inst/alloc 0)
+        (vector-make/recur n 0 (inst/alloc n) x)))
+    """)
+    emitter = FunctionEmitter(env)
+    for definition in code:
+        emitter.visit(definition)
 
 
 def add_prelude(env: Dict[SSym, Value]) -> None:
