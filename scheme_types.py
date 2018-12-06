b23d11a77e231c7ef7b1259b53afbfc9377d7dfe
James Perretta <jameslp@umich.edu>
2018-12-06 18:46:52 -0500

    Finished basic type checking. Given a dictionary of param types and the signatures of builtin functions, the FunctionTypeAnalyzer computes the types of all body expressions and the return type of the function.

--- scheme_types.py
+++ scheme_types.py
@@ -1,5 +1,6 @@
+import copy
 from dataclasses import dataclass
-from typing import Any, Dict, Optional, Tuple, Type
+from typing import Any, Dict, Mapping, Optional, Tuple, Type
 
 import sexp
 from visitor import Visitor
@@ -53,6 +54,7 @@ class SchemeVectType(SchemeObjectType):
 @dataclass(frozen=True)
 class SchemeFunctionType(SchemeObjectType):
     arity: Optional[int]
+    return_type: SchemeObjectType = SchemeObject
 
 
 @dataclass(frozen=True)
@@ -75,36 +77,57 @@ class SExpWrapper:
 
 
 class FunctionTypeAnalyzer(Visitor):
-    def __init__(self, param_types: Dict[sexp.SSym, SchemeObjectType]):
+    def __init__(self, param_types: Mapping[sexp.SSym, SchemeObjectType]):
         self._param_types = param_types
         self._expr_types: Dict[SExpWrapper, SchemeObjectType] = {}
 
+        self._function_type: Optional[SchemeFunctionType] = None
+
+    def get_function_type(self) -> SchemeFunctionType:
+        assert self._function_type is not None
+        return self._function_type
+
+    def get_expr_types(self) -> Dict[SExpWrapper, SchemeObjectType]:
+        return copy.copy(self._expr_types)
+
+    def _get_expr_type(self, expr: sexp.SExp) -> SchemeObjectType:
+        return self._expr_types[SExpWrapper(expr)]
+
+    def _set_expr_type(self, expr: sexp.SExp, type_: SchemeObjectType) -> None:
+        self._expr_types[SExpWrapper(expr)] = type_
+
+    def _expr_type_known(self, expr: sexp.SExp) -> bool:
+        return SExpWrapper(expr) in self._expr_types
+
     def visit_SFunction(self, func: sexp.SFunction) -> None:
         if func.is_lambda:
-            self._expr_types[SExpWrapper(func)] = (
-                SchemeFunctionType(len(func.params)))
+            self._set_expr_type(func, SchemeFunctionType(len(func.params)))
             # Lambda bodies will be analyzed separately when they're called
         else:
             super().visit(func.body)
 
+            self._function_type = SchemeFunctionType(
+                len(func.params),
+                self._get_expr_type(list(func.body)[-1])
+            )
+            self._set_expr_type(func, self._function_type)
+
     def visit_SNum(self, num: sexp.SNum) -> None:
-        self._expr_types[SExpWrapper(num)] = SchemeNumType()
+        self._set_expr_type(num, SchemeNumType())
 
     def visit_SBool(self, sbool: sexp.SBool) -> None:
-        self._expr_types[SExpWrapper(sbool)] = SchemeBoolType()
+        self._set_expr_type(sbool, SchemeBoolType())
 
     def visit_SSym(self, sym: sexp.SSym) -> None:
         if sym in self._param_types:
-            self._expr_types[SExpWrapper(sym)] = self._param_types[sym]
-        elif sym in _BUILTINS_RETURN_TYPES:
-            self._expr_types[SExpWrapper(sym)] = _BUILTINS_RETURN_TYPES[sym]
+            self._set_expr_type(sym, self._param_types[sym])
         elif sym in _BUILTINS_FUNC_TYPES:
-            self._expr_types[SExpWrapper(sym)] = _BUILTINS_FUNC_TYPES[sym]
+            self._set_expr_type(sym, _BUILTINS_FUNC_TYPES[sym])
         else:
-            self._expr_types[SExpWrapper(sym)] = SchemeSym
+            self._set_expr_type(sym, SchemeObject)
 
     def visit_SVect(self, vect: sexp.SVect) -> None:
-        self._expr_types[SExpWrapper(vect)] = SchemeVectType(len(vect.items))
+        self._set_expr_type(vect, SchemeVectType(len(vect.items)))
 
     def visit_Quote(self, quote: sexp.Quote) -> None:
         type_: SchemeObjectType
@@ -114,127 +137,81 @@ class FunctionTypeAnalyzer(Visitor):
         else:
             type_ = SchemeQuotedType(type(quote.expr))
 
-        self._expr_types[SExpWrapper(quote)] = type_
+        self._set_expr_type(quote, type_)
 
     def visit_SCall(self, call: sexp.SCall) -> None:
         super().visit_SCall(call)
-        assert False, 'fixme: builtin func return types'
+        if self._expr_type_known(call.func):
+            func_type = self._get_expr_type(call.func)
+            if isinstance(func_type, SchemeFunctionType):
+                self._set_expr_type(call, func_type.return_type)
+            else:
+                self._set_expr_type(call, SchemeObject)
+        else:
+            self._set_expr_type(call, SchemeObject)
 
     def visit_SConditional(self, cond: sexp.SConditional) -> None:
         super().visit_SConditional(cond)
 
-        then_type = self._expr_types[SExpWrapper(cond.then_expr)]
-        else_type = self._expr_types[SExpWrapper(cond.else_expr)]
+        then_type = self._get_expr_type(cond.then_expr)
+        else_type = self._get_expr_type(cond.else_expr)
         if then_type == else_type:
-            self._expr_types[SExpWrapper(cond)] = then_type
+            self._set_expr_type(cond, then_type)
         else:
-            self._expr_types[SExpWrapper(cond)] = SchemeObjectType()
+            self._set_expr_type(cond, SchemeObjectType())
 
-
-_BUILTINS_RETURN_TYPES: Dict[sexp.SSym, SchemeObjectType] = {
-    sexp.SSym('inst/typeof'): SchemeSym,
-    sexp.SSym('inst/trap'): SchemeBottom,
-    sexp.SSym('inst/trace'): SchemeNum,
-    sexp.SSym('inst/breakpoint'): SchemeNum,
-    sexp.SSym('inst/alloc'): SchemeVectType(None),
-    sexp.SSym('inst/load'): SchemeObject,
-    sexp.SSym('inst/store'): SchemeVectType(None),
-    sexp.SSym('inst/length'): SchemeNum,
-
-    sexp.SSym('inst/+'): SchemeNum,
-    sexp.SSym('inst/-'): SchemeNum,
-    sexp.SSym('inst/*'): SchemeNum,
-    sexp.SSym('inst//'): SchemeNum,
-    sexp.SSym('inst/%'): SchemeNum,
-    sexp.SSym('inst/number='): SchemeBool,
-    sexp.SSym('inst/symbol='): SchemeBool,
-    sexp.SSym('inst/pointer='): SchemeBool,
-    sexp.SSym('inst/number<'): SchemeBool,
-
-    sexp.SSym('trap'): SchemeBottom,
-    sexp.SSym('trace'): SchemeNum,
-    sexp.SSym('breakpoint'): SchemeNum,
-    sexp.SSym('assert'): SchemeNum,
-    sexp.SSym('typeof'): SchemeSym,
-    sexp.SSym('not'): SchemeBool,
-
-    sexp.SSym('number?'): SchemeBool,
-    sexp.SSym('symbol?'): SchemeBool,
-    sexp.SSym('vector?'): SchemeBool,
-    sexp.SSym('function?'): SchemeBool,
-    sexp.SSym('bool?'): SchemeBool,
-    sexp.SSym('pair?'): SchemeBool,
-    sexp.SSym('nil?'): SchemeBool,
-
-    sexp.SSym('+'): SchemeNum,
-    sexp.SSym('-'): SchemeNum,
-    sexp.SSym('*'): SchemeNum,
-    sexp.SSym('/'): SchemeNum,
-    sexp.SSym('%'): SchemeNum,
-
-    sexp.SSym('pointer='): SchemeBool,
-    sexp.SSym('symbol='): SchemeBool,
-    sexp.SSym('number='): SchemeBool,
-    sexp.SSym('number<'): SchemeBool,
-
-    sexp.SSym('vector-length'): SchemeNum,
-    sexp.SSym('vector-index'): SchemeObject,
-
-    sexp.SSym('vector-set!'): SchemeVectType(None),
-    sexp.SSym('vector-make/recur'): SchemeVectType(None),
-    sexp.SSym('vector-make'): SchemeVectType(None),
-}
 
 _BUILTINS_FUNC_TYPES: Dict[sexp.SSym, SchemeObjectType] = {
-    sexp.SSym('inst/typeof'):  SchemeFunctionType(1),
-    sexp.SSym('inst/trap'): SchemeFunctionType(0),
-    sexp.SSym('inst/trace'): SchemeFunctionType(1),
-    sexp.SSym('inst/breakpoint'): SchemeFunctionType(0),
-    sexp.SSym('inst/alloc'): SchemeFunctionType(1),
-    sexp.SSym('inst/load'): SchemeFunctionType(2),
-    sexp.SSym('inst/store'): SchemeFunctionType(3),
-    sexp.SSym('inst/length'): SchemeFunctionType(1),
+    sexp.SSym('inst/typeof'):  SchemeFunctionType(1, SchemeSym),
+    sexp.SSym('inst/trap'): SchemeFunctionType(0, SchemeBottom),
+    sexp.SSym('inst/trace'): SchemeFunctionType(1, SchemeNum),
+    sexp.SSym('inst/breakpoint'): SchemeFunctionType(0, SchemeNum),
+    sexp.SSym('inst/alloc'): SchemeFunctionType(1, SchemeVectType(None)),
+    sexp.SSym('inst/load'): SchemeFunctionType(2, SchemeObject),
+    sexp.SSym('inst/store'): SchemeFunctionType(3, SchemeVectType(None)),
+    sexp.SSym('inst/length'): SchemeFunctionType(1, SchemeNum),
 
-    sexp.SSym('inst/+'): SchemeFunctionType(2),
-    sexp.SSym('inst/-'): SchemeFunctionType(2),
-    sexp.SSym('inst/*'): SchemeFunctionType(2),
-    sexp.SSym('inst//'): SchemeFunctionType(2),
-    sexp.SSym('inst/%'): SchemeFunctionType(2),
-    sexp.SSym('inst/number='): SchemeFunctionType(2),
-    sexp.SSym('inst/symbol='): SchemeFunctionType(2),
-    sexp.SSym('inst/pointer='): SchemeFunctionType(2),
-    sexp.SSym('inst/number<'): SchemeFunctionType(2),
+    sexp.SSym('inst/+'): SchemeFunctionType(2, SchemeNum),
+    sexp.SSym('inst/-'): SchemeFunctionType(2, SchemeNum),
+    sexp.SSym('inst/*'): SchemeFunctionType(2, SchemeNum),
+    sexp.SSym('inst//'): SchemeFunctionType(2, SchemeNum),
+    sexp.SSym('inst/%'): SchemeFunctionType(2, SchemeNum),
+    sexp.SSym('inst/number='): SchemeFunctionType(2, SchemeBool),
+    sexp.SSym('inst/symbol='): SchemeFunctionType(2, SchemeBool),
+    sexp.SSym('inst/pointer='): SchemeFunctionType(2, SchemeBool),
+    sexp.SSym('inst/number<'): SchemeFunctionType(2, SchemeBool),
 
-    sexp.SSym('trap'): SchemeFunctionType(0),
-    sexp.SSym('trace'): SchemeFunctionType(1),
-    sexp.SSym('breakpoint'): SchemeFunctionType(0),
-    sexp.SSym('assert'): SchemeFunctionType(1),
-    sexp.SSym('typeof'): SchemeFunctionType(1),
-    sexp.SSym('not'): SchemeFunctionType(1),
+    sexp.SSym('trap'): SchemeFunctionType(0, SchemeBottom),
+    sexp.SSym('trace'): SchemeFunctionType(1, SchemeNum),
+    sexp.SSym('breakpoint'): SchemeFunctionType(0, SchemeNum),
+    sexp.SSym('assert'): SchemeFunctionType(1, SchemeNum),
+    sexp.SSym('typeof'): SchemeFunctionType(1, SchemeSym),
+    sexp.SSym('not'): SchemeFunctionType(1, SchemeBool),
 
-    sexp.SSym('number?'): SchemeFunctionType(1),
-    sexp.SSym('symbol?'): SchemeFunctionType(1),
-    sexp.SSym('vector?'): SchemeFunctionType(1),
-    sexp.SSym('function?'): SchemeFunctionType(1),
-    sexp.SSym('bool?'): SchemeFunctionType(1),
-    sexp.SSym('pair?'): SchemeFunctionType(1),
-    sexp.SSym('nil?'): SchemeFunctionType(1),
+    sexp.SSym('number?'): SchemeFunctionType(1, SchemeBool),
+    sexp.SSym('symbol?'): SchemeFunctionType(1, SchemeBool),
+    sexp.SSym('vector?'): SchemeFunctionType(1, SchemeBool),
+    sexp.SSym('function?'): SchemeFunctionType(1, SchemeBool),
+    sexp.SSym('bool?'): SchemeFunctionType(1, SchemeBool),
+    sexp.SSym('pair?'): SchemeFunctionType(1, SchemeBool),
+    sexp.SSym('nil?'): SchemeFunctionType(1, SchemeBool),
 
-    sexp.SSym('+'): SchemeFunctionType(2),
-    sexp.SSym('-'): SchemeFunctionType(2),
-    sexp.SSym('*'): SchemeFunctionType(2),
-    sexp.SSym('/'): SchemeFunctionType(2),
-    sexp.SSym('%'): SchemeFunctionType(2),
+    sexp.SSym('+'): SchemeFunctionType(2, SchemeNum),
+    sexp.SSym('-'): SchemeFunctionType(2, SchemeNum),
+    sexp.SSym('*'): SchemeFunctionType(2, SchemeNum),
+    sexp.SSym('/'): SchemeFunctionType(2, SchemeNum),
+    sexp.SSym('%'): SchemeFunctionType(2, SchemeNum),
 
-    sexp.SSym('pointer='): SchemeFunctionType(2),
-    sexp.SSym('symbol='): SchemeFunctionType(2),
-    sexp.SSym('number='): SchemeFunctionType(2),
-    sexp.SSym('number<'): SchemeFunctionType(2),
+    sexp.SSym('pointer='): SchemeFunctionType(2, SchemeBool),
+    sexp.SSym('symbol='): SchemeFunctionType(2, SchemeBool),
+    sexp.SSym('number='): SchemeFunctionType(2, SchemeBool),
+    sexp.SSym('number<'): SchemeFunctionType(2, SchemeBool),
 
-    sexp.SSym('vector-length'): SchemeFunctionType(1),
-    sexp.SSym('vector-index'): SchemeFunctionType(2),
+    sexp.SSym('vector-length'): SchemeFunctionType(1, SchemeNum),
+    sexp.SSym('vector-index'): SchemeFunctionType(2, SchemeObject),
 
-    sexp.SSym('vector-set!'): SchemeFunctionType(3),
-    sexp.SSym('vector-make/recur'): SchemeFunctionType(4),
-    sexp.SSym('vector-make'): SchemeFunctionType(2),
+    sexp.SSym('vector-set!'): SchemeFunctionType(3, SchemeVectType(None)),
+    sexp.SSym('vector-make/recur'): (
+        SchemeFunctionType(4, SchemeVectType(None))),
+    sexp.SSym('vector-make'): SchemeFunctionType(2, SchemeVectType(None)),
 }
